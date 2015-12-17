72406daf8061c3807bb7adebe6f6197a34833cc0
kolyas <kolyas@gmail.com>
2015-12-18 00:12:37 +0600

    Decided level04/lesson04/TASK08

---
+++ src/com/javarush/test/level04/lesson04/task08/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.test.level04.lesson04.task08;
+
+/* Треугольник
+Ввести с клавиатуры три числа а, b, c – стороны предполагаемого треугольника.
+Определить возможность существования треугольника по сторонам. Результат вывести на экран в следующем виде:
+"Треугольник существует." - если треугольник с такими сторонами существует.
+"Треугольник не существует." - если треугольник с такими сторонами не существует.
+Подсказка: Треугольник существует только тогда, когда сумма любых двух его сторон больше третьей.
+Требуется сравнить каждую сторону с суммой двух других.
+Если хотя бы в одном случае сторона окажется больше суммы двух других, то треугольника с такими сторонами не существует.
+*/
+
+import java.io.*;
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        //напишите тут ваш код
+        InputStream is = System.in;
+        Reader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+
+        String s1 = br.readLine();
+        String s2 = br.readLine();
+        String s3 = br.readLine();
+
+        int a = Integer.parseInt(s1);
+        int b = Integer.parseInt(s2);
+        int c = Integer.parseInt(s3);
+
+        if ( (a+b)>c & (b+c)>a & (a+c)>b ) {
+            System.out.println("Треугольник существует.");
+        }
+        else {
+            System.out.println("Треугольник не существует.");
+        }
+
+    }
+}

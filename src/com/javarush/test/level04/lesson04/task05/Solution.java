bf2a78846d20bc326d9f021bff4fd7b0e1055e25
kolyas <kolyas@gmail.com>
2015-12-17 22:39:54 +0600

    Decided level04/lesson04/TASK05

---
+++ src/com/javarush/test/level04/lesson04/task05/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.test.level04.lesson04.task05;
+
+/* Положительное и отрицательное число
+Ввести с клавиатуры число. Если число положительное, то увеличить его в два раза. Если число отрицательное, то прибавить единицу.
+Вывести результат на экран.
+*/
+
+import java.io.*;
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        //напишите тут ваш код
+        InputStream is = System.in;
+        Reader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+
+        String d = br.readLine();
+        int a = Integer.parseInt(d);
+
+        if (a>=0){
+            a*=2;
+            System.out.println(a);
+        }
+        else {
+            a+=1;
+            System.out.println(a);
+        }
+
+    }
+
+}

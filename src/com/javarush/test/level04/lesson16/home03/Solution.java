490579ff985321ecd0fc553b77977fd47c4fec51
kolyas <kolyas@gmail.com>
2015-12-19 12:22:25 +0600

    Finish level04

--- src/com/javarush/test/level04/lesson16/home03/Solution.java
+++ src/com/javarush/test/level04/lesson16/home03/Solution.java
@@ -11,5 +11,14 @@ public class Solution
     public static void main(String[] args)   throws Exception
     {
         //напишите тут ваш код
+        int sum = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            String s = br.readLine();
+            int a = Integer.parseInt(s);
+            sum+=a;
+            if (a==-1) break;
+        }
+        System.out.println(sum);
     }
 }

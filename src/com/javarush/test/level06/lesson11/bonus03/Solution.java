7a9e94b0c0899174eba8900be09f3be90469b7c5
kolyas <kolyas@kolyas.com>
2015-12-24 08:48:01 +0600

    Decided level06

--- src/com/javarush/test/level06/lesson11/bonus03/Solution.java
+++ src/com/javarush/test/level06/lesson11/bonus03/Solution.java
@@ -2,7 +2,6 @@ package com.javarush.test.level06.lesson11.bonus03;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 /* Задача по алгоритмам
 Задача: Написать программу, которая вводит с клавиатуры 5 чисел и выводит их в возрастающем порядке.
@@ -22,19 +21,36 @@ import java.util.ArrayList;
 
 public class Solution
 {
-    public static ArrayList<Integer> cats = new ArrayList<>();
+
 
     public static void main(String[] args) throws Exception
     {
+        int[] cats = new int[5];
         BufferedReader reader  = new BufferedReader(new InputStreamReader(System.in));
-        for (int i = 0; i<5; i++)
+//        for (int i = 0; i<cats.length; i++)
+//        {
+//            int a = Integer.parseInt(reader.readLine());
+//            cats[i] = a;
+//        }
+        cats[0] = Integer.parseInt(reader.readLine());
+        cats[1] = Integer.parseInt(reader.readLine());
+        cats[2] = Integer.parseInt(reader.readLine());
+        cats[3] = Integer.parseInt(reader.readLine());
+        cats[4] = Integer.parseInt(reader.readLine());
+
+        for (int i = cats.length - 1; i >= 1; i--) {
+            for (int j = 0; j < i; j++) {
+                if (cats[j] > cats[j+1]) {
+                    int temp = cats[j];
+                    cats[j] = cats[j+1];
+                    cats[j+1] = temp;
+                }
+            }
+        }
+        for (int cat : cats)
         {
-            int a = Integer.parseInt(reader.readLine());
-            Solution.cats.add(a);
+            System.out.println(cat);
         }
-        for (int j = 0; j<Solution.cats.size();j++){
-            for ()
-            System.out.println(Solution.cats.get(j));
-        }
+
     }
 }
